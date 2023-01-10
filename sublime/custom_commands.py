b1b9d986b6bddaaf471a02fded76799c47f4c4c4
Brandon Chinn <brandonchinn178@gmail.com>
2023-01-09 16:54:51 -0800

    Add open file in github command

--- sublime/custom_commands.py
+++ sublime/custom_commands.py
@@ -1,5 +1,7 @@
-import os
+from pathlib import Path
+import re
 import subprocess
+import webbrowser
 
 import sublime
 from sublime_plugin import WindowCommand
@@ -14,18 +16,53 @@ class OpenAllModifiedCommand(WindowCommand):
         if len(folders) == 0:
             sublime.error_message('ERROR: No folders open.')
             return
-        folder = folders[0]
+        folder = Path(folders[0])
 
         diff_args = ['--staged'] if staged else []
 
-        proc = subprocess.Popen(
-            ['git', 'diff', '--name-only'] + diff_args,
+        proc = subprocess.run(
+            ['git', 'diff', '--diff-filter=dxb', '--name-only'] + diff_args,
             cwd=folder,
-            stdout=subprocess.PIPE,
+            capture_output=True,
+            encoding='utf-8',
         )
-        (stdout, _) = proc.communicate()
 
-        modified_files = stdout.decode('utf-8').split('\n')
+        modified_files = proc.stdout.split('\n')
         for file in modified_files:
             if file:
-                self.window.open_file(os.path.join(folder, file))
+                self.window.open_file((folder / file).as_posix())
+
+
+class OpenInGithubCommand(WindowCommand):
+    """Defines a command that opens the current file in GitHub."""
+
+    def run(self):
+        sheet = self.window.active_sheet()
+        if not sheet:
+            sublime.error_message('ERROR: no tab open.')
+            return
+
+        file = sheet.file_name()
+        if not file:
+            sublime.error_message('ERROR: no file open.')
+            return
+        file = Path(file)
+
+        proc = subprocess.run(
+            ['git', 'rev-parse', '--show-toplevel'],
+            cwd=file.parent,
+            capture_output=True,
+            encoding='utf-8',
+        )
+        gitdir = Path(proc.stdout.strip())
+        file = file.relative_to(gitdir)
+
+        proc = subprocess.run(
+            ['git', 'remote', '-v'],
+            cwd=gitdir,
+            capture_output=True,
+            encoding='utf-8',
+        )
+        repo = re.search(r'git@github\.com:(\w+/\w+)', proc.stdout)[1]
+        url = f'https://github.com/{repo}/blob/main/{file.as_posix()}'
+        webbrowser.open_new_tab(url)
