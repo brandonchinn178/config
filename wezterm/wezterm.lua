7bc096efef4cc018dcf81657337139bea24c426f
Brandon Chinn <brandonchinn178@gmail.com>
2023-04-16 15:39:57 -0700

    Fix font-size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -130,7 +130,7 @@ wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_wid
 end)
 
 return {
-  font_size = 15, -- 16 results in COLUMNS=78 when half-screen in monitor
+  font_size = 14, -- set this to keep $COLUMNS >= 80 when half-screen
   audible_bell = 'Disabled',
   enable_scroll_bar = true,
 
