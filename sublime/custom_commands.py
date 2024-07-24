99cc63f5b6a98e343b2db06b55b3309380162970
Brandon Chinn <brandonchinn178@gmail.com>
2024-07-24 15:11:48 -0700

    Fix "Open in GitHub" for non-cloned repos

--- sublime/custom_commands.py
+++ sublime/custom_commands.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import re
 import subprocess
 import webbrowser
 
@@ -14,8 +13,7 @@ class OpenAllModifiedCommand(WindowCommand):
     def run(self, staged=False):
         folders = self.window.folders()
         if len(folders) == 0:
-            sublime.error_message('ERROR: No folders open.')
-            return
+            fail('ERROR: No folders open.')
         folder = Path(folders[0])
 
         diff_args = ['--staged'] if staged else []
@@ -39,41 +37,67 @@ class OpenInGithubCommand(WindowCommand):
     def run(self):
         sheet = self.window.active_sheet()
         if not sheet:
-            sublime.error_message('ERROR: no tab open.')
-            return
+            fail('ERROR: no tab open.')
 
         file = sheet.file_name()
         if not file:
-            sublime.error_message('ERROR: no file open.')
-            return
+            fail('ERROR: no file open.')
         file = Path(file)
 
-        proc = subprocess.run(
+        gitdir = self._get_output(
             ['git', 'rev-parse', '--show-toplevel'],
             cwd=file.parent,
-            capture_output=True,
-            encoding='utf-8',
-            check=True,
+            error='Could not get repo directory',
         )
-        gitdir = Path(proc.stdout.strip())
+        gitdir = Path(gitdir.strip())
         file = file.relative_to(gitdir)
 
-        proc = subprocess.run(
-            ['git', 'rev-parse', '--abbrev-ref', 'origin/HEAD'],
-            cwd=gitdir,
-            capture_output=True,
-            encoding='utf-8',
-            check=True,
-        )
-        default_branch = proc.stdout.strip().split('/')[1]
-
-        proc = subprocess.run(
+        remotes = self._get_output(
             ['git', 'remote', '-v'],
             cwd=gitdir,
-            capture_output=True,
-            encoding='utf-8',
-            check=True,
+            error='Could not get remotes',
         )
-        repo = re.search(r'git@github\.com:(\w+/[\w-]+)(\.git)?', proc.stdout)[1]
+        remote_name, remote_url, _ = remotes.splitlines()[0].split()
+
+        repo = remote_url.split(":")[1]
+        default_branch = self._get_default_branch(gitdir, remote_name)
         url = f'https://github.com/{repo}/blob/{default_branch}/{file.as_posix()}'
         webbrowser.open_new_tab(url)
+
+    def _get_default_branch(self, gitdir, remote_name):
+        # this file only exists if we git cloned this repo originally
+        head_ref_file = gitdir / ".git/refs/remotes" / remote_name / "HEAD"
+        if not head_ref_file.exists():
+            out = self._get_output(
+                ['git', 'ls-remote', '--symref', remote_name],
+                cwd=gitdir,
+                error=f"Could not fetch refs for {remote_name}",
+            )
+            head_ref = next(
+                line.rsplit(maxsplit=1)[0]
+                for line in out.splitlines()
+                if line.endswith("HEAD") and line.startswith('ref:')
+            )
+            head_ref_file.write_text(head_ref)
+
+        return self._get_output(
+            ['git', 'rev-parse', '--abbrev-ref', 'origin/HEAD'],
+            cwd=gitdir,
+            error='Could not get default branch',
+        ).strip()
+
+    def _get_output(self, args, *, error, **kwargs):
+        kwargs = {
+            "capture_output": True,
+            "encoding": "utf-8",
+            **kwargs,
+        }
+        proc = subprocess.run(args, **kwargs)
+        if proc.returncode != 0:
+            fail(f'ERROR: {error}\n{proc.stdout}\n{proc.stderr}')
+
+        return proc.stdout
+
+def fail(msg):
+    sublime.error_message(msg)
+    raise Exception
